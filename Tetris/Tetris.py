3b34da043a753b8e339ef4dc27b7f92997740fe3
Alex Liao <92276003+piescape@users.noreply.github.com>
2021-10-19 21:02:24 -0700

    Update Tetris.py
    Changed it so you can rotate with Z and X as well as including in the Standard Rotation System (SRS) which can allow pieces to rotate in a much better manner.

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -3,15 +3,13 @@
 import pygame
 import random
 
-colors = [
-    (0, 0, 0),
+colors = [(0, 0, 0),
     (120, 37, 179),
     (100, 179, 179),
     (80, 34, 22),
     (80, 134, 22),
     (180, 34, 22),
-    (180, 34, 122),
-]
+    (180, 34, 122),]
 
 
 class Figure:
@@ -19,13 +17,13 @@ class Figure:
     y = 0
 
     figures = [
-        [[1, 5, 9, 13], [4, 5, 6, 7]],
-        [[4, 5, 9, 10], [2, 6, 5, 9]],
-        [[6, 7, 9, 10], [1, 5, 6, 10]],
-        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
-        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
-        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
-        [[1, 2, 5, 6]],
+    [[0, 1, 2, 3], [1, 5, 9, 13], [4, 5, 6, 7], [2, 6, 10, 14]], #I piece
+    [[4, 5, 9, 10], [2, 6, 5, 9], [0, 1, 5, 6], [1, 5, 4, 8]], #Z piece
+    [[1, 2, 4, 5], [0, 4, 5, 9], [5, 6, 8, 9], [1, 5, 6, 10]], #S piece
+    [[0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10], [1, 2, 5, 9]], #J piece
+    [[3, 5, 6, 7], [1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11]], #L piece
+    [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]], #T piece
+    [[1, 2, 5, 6]], #O piece
     ]
 
     def __init__(self, x, y):
@@ -38,8 +36,10 @@ class Figure:
     def image(self):
         return self.figures[self.type][self.rotation]
 
-    def rotate(self):
+    def lrotate(self):
         self.rotation = (self.rotation + 1) % len(self.figures[self.type])
+    def rrotate(self):
+        self.rotation = (self.rotation - 1) % len(self.figures[self.type])
 
 
 class Tetris:
@@ -123,9 +123,14 @@ class Tetris:
         if self.intersects():
             self.figure.x = old_x
 
-    def rotate(self):
+    def lrotate(self):
         old_rotation = self.figure.rotation
-        self.figure.rotate()
+        self.figure.lrotate()
+        if self.intersects():
+            self.figure.rotation = old_rotation
+    def rrotate(self):
+        old_rotation = self.figure.rotation
+        self.figure.rrotate()
         if self.intersects():
             self.figure.rotation = old_rotation
 
@@ -167,8 +172,10 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                game.rotate()
+            if event.key == pygame.K_z:
+                game.lrotate()
+            if event.key == pygame.K_x:
+                game.rrotate()
             if event.key == pygame.K_DOWN:
                 pressing_down = True
             if event.key == pygame.K_LEFT:
@@ -218,3 +225,4 @@ while not done:
     clock.tick(fps)
 
 pygame.quit()
+
