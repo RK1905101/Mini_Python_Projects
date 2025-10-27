2c412267e20fd20ddd5c5a8e709e8c30faa7fb49
Deenu Bansal <bansaldeenu33@gmail.com>
2025-10-27 07:28:42 +0530

    feat: Add strong password generator script

--- passwordGenerator.py
+++
@@ -1,5 +0,0 @@
-import random
-length = int(input("Enter the length of password: "))
-char = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p = "".join(random.sample(char, length))
-print(p)
